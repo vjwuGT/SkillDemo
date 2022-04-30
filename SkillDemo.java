717cff179d9f6b425b7f1bb8ae917ccf255e47df
wuvic21 <victorwu689@gmail.com>
2022-04-29 23:57:55 -0700

    added a sum method to SkillDemo.java, added .jar files for junit, and made a SkillDemoTest.java to run junit tests

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,2 +1,9 @@
 public class SkillDemo{
+    public int sum(int[] array){
+        int total = 0;
+        for (int num : array){
+            total += num;
+        }
+        return total;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SkillDemoTest {
+    
+    @Test
+    public void test1(){
+        SkillDemo sd = new SkillDemo();
+        int[] array = {1,2,3,4};
+        assertEquals(10, sd.sum(array)+1);
+    }
+}
