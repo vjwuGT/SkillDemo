054e37faf1bb49233f66670fa5ccf64df564ea71
wuvic21 <victorwu689@gmail.com>
2022-04-30 00:00:07 -0700

    fixed the error with the test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -7,6 +7,6 @@ public class SkillDemoTest {
     public void test1(){
         SkillDemo sd = new SkillDemo();
         int[] array = {1,2,3,4};
-        assertEquals(10, sd.sum(array)+1);
+        assertEquals(10, sd.sum(array));
     }
 }
